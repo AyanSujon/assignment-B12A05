d572030d8da3aeb7bd4dea2b227c2ab1d82f5809
Ayan Sujon <ayansujonbd@gmail.com>
2025-08-27 23:27:01 +0600

    Navbar Heart Icons Functionalities added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-console.log("Hello JS");
+// Navbar Heart Icons Functionalities // 
+
+// Navbar heart counter ( inside .heart-icon > #heart-icon-count")
+const heartCounter = document.getElementById("heart-icon-count");
+
+// Select all card heart icons (inside .card > div > #card-heart-icon)
+const heartIcons = document.querySelectorAll("#card-heart-icon");
+
+// Loop with for-of
+for (const icon of heartIcons) {
+  icon.addEventListener("click", function() {
+let heartCounterConverted = parseInt(heartCounter.innerText);
+    heartCounter.innerText = heartCounterConverted + 1;
+  });
+}
+
+// Card Call Buttons Functionalities
+
+
+
